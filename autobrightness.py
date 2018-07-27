efd8c640e099efa93ba0a714bbba3a2aac24789e
Daniel W. Lu <dandydanny@gmail.com>
2018-07-27 16:16:22 -0400

    Added auto-dimming script

---
+++ autobrightness.py
@@ -0,0 +1,36 @@
+from gpiozero import LightSensor
+from time import sleep
+
+# Desired maximum & minimum backlight values
+max = 255
+min = 12
+prev = 0
+offset = 10
+delta = 0
+
+# Open backlight control 'file' for writing
+f = open('/sys/class/backlight/rpi_backlight/brightness', 'w')
+
+# Instantiate sensor object, set charge time suitable for 10uF capacitor
+sensor = LightSensor(18, charge_time_limit=0.2, threshold=0.1)
+
+while True:
+    # Get light sensor value, map to 0-255
+    lightValue = round(255 * sensor.value + offset)
+
+    # Keep values within defined maximum-minimum values
+    if lightValue > 240:
+        lightValue = max
+    if lightValue < min:
+        lightValue = min
+    
+    # Calculate change in brightness value
+    delta = abs(lightValue - prev)
+
+    # Only write (change) backlight value if there's sensor level change
+    if delta > 5:
+        prev = lightValue
+        f.seek(0)
+        f.write(str(lightValue))
+        f.truncate()
+sleep(1)
